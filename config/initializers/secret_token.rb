5d6fc28c3a354b5af58ea887cd8742bbc703b03d
Austin Keeley <austin.keeley@gmail.com>
2013-06-14 18:46:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlashthriftWeb::Application.config.secret_token = 'e399c874751a73453253110c0faf524352c9acfd9e217ff12210c4eb71782867e007394b693c6c2f0db2a1a0963a92ffb805263e497e51fd9befda9627be6ee2'
