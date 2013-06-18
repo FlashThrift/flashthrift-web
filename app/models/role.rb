5d2eabb387c22e22f4c82efb7c26e3691a120871
Austin Keeley <austin.keeley@gmail.com>
2013-06-18 18:00:01 -0400

    Updating role model

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,8 @@
+=begin
+  A Role is a way to distinguish between lowly pathetic users (who can only post items),
+  moderators (who can remove items and suspend user accounts), sales team (who are moderators
+  who can also view transactions) and the admin group, who pretty much run this town.
+=end
 class Role < ActiveRecord::Base
-  attr_accessible :role_description, :role_name
+  attr_readonly :name, :role_description
 end
