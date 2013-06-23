f0627abc27df5185deb3aede72322ce1e9771108
Austin Keeley <austin.keeley@gmail.com>
2013-06-23 11:16:47 -0400

    Switching to ruby 1.8 syntax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,21 @@
 class UsersController < ApplicationController
+  
+  # Show form to make a new user
   def new
     @user = User.new
   end
 
+  # List all the users
   def index
     @users = User.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json {render json: @users }
+      format.json {render :json => @users }
     end
   end
 
+  # Creates a new user
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -29,7 +33,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @item }
+      format.json { render :json => @item }
     end
   end
 
