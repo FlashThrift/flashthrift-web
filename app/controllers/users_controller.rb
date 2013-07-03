3ffb6346c281ca49465de0ee4d6eabc5eef3fdc1
Austin Keeley <austin.keeley@gmail.com>
2013-07-02 23:08:33 -0400

    Adding user update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,5 +42,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  # Updates a user
+  # Note that roles can only be updated by an admin
+  def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, :notice => 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render :action => "edit" }
+        format.json { render :json => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 
 end
